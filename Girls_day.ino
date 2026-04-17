86ea75445eb9cfe8c5c5d712d729903ce3b6bbcd
Mario jesus FLORES ORIHUELA <mj.floresorihuela@technologyandstrategy.com>
2026-04-17 11:06:24 +0200

    ServoMotor

--- Girls_day.ino
+++ Girls_day.ino
@@ -1,19 +1,33 @@
+
 #include <RH_ASK.h>
 #include <SPI.h> // not used, but important for getting fonctions for make it work
+#include <ServoTimer2.h>
 
+#define rollPin  2
+int pos = 0; 
 RH_ASK driver; // Config 2000bps, TX pin 12, RX  pin 11
+ServoTimer2 miServo; 
+
 
 #define Mon_ID "100"
 #define RECIVER_ID "300"
 
 void setup() {
+
+    miServo.attach(rollPin);     
+    
     Serial.begin(9600);
+    
     if (!driver.init()) {
+    
         Serial.println("Init not working");
+    
     }
+ 
 }
 
 void loop() {
+    //incPulse();
     Sending_message();
     reading_message();
 }
@@ -53,6 +67,47 @@ void reading_message(){
         if(id_read != NULL && strcmp(id_read, Mon_ID) == 0){
             Serial.print("Recive: ");
             Serial.println(message); 
+            
+            if(strcmp(message, "OPEN") == 0){
+                Serial.println("Opening doors");
+                int angle = 90;
+                int anchoPulso = map(angle, 0, 180, 750, 2800);
+                miServo.write(anchoPulso);
+
+            }else if(strcmp(message, "CLOSE") == 0){
+                
+                Serial.println("Closing doors");
+                int angle = 0;
+                int anchoPulso = map(angle, 0, 180, 750, 2800);
+                miServo.write(anchoPulso);
+
+            }
         }
     }
+}
+
+int incPulse(){
+   if (Serial.available() > 0) {
+    
+    int angle = Serial.parseInt();
+    
+    while(Serial.available() > 0) {
+      Serial.read();
+    }
+    
+    
+    if (angle >= 0 && angle <= 180) {
+      Serial.print("Moving servo: ");
+      Serial.print(angle);
+      Serial.println(" degrees.");
+      
+    
+      int anchoPulso = map(angle, 0, 180, 750, 2800);
+      
+      miServo.write(anchoPulso);
+      
+    } else {
+      Serial.println("Error: anglee out of range");
+    }
+  } 
 }
